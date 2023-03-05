ffe7a482311292e183184f85e146d7d7d13b4b9c
mgonzalz <114707509+mgonzalz@users.noreply.github.com>
2023-03-05 16:40:18 +0000

    ejercicio12

--- ejercicio12/ejercicio12.1.py
+++ ejercicio12/ejercicio12.1.py
@@ -1,17 +1,28 @@
 def cuadrados_perfectos(limite):
   cuadrados = []
-  for i in range(limite):
-    if i**2 < limite:
-      cuadrados.append(i**2)  #Añadir los cuadrados
-    else:
-      return cuadrados
+  i = 0
+  while i**2 <= limite:  #bucle iterativo
+    cuadrados.append(i**2)
+    i += 1
+  return cuadrados
+
+
+def raices(numero):  #r^2 ≤ n < (r + 1)^2
+  i = 0
+  while i**2 <= numero:
+    i += 1
+    if i**2 == numero:
+      return i
 
 
 if __name__ == "__main__":
-  limite = input("Introduzca un límite: ")
-  try:
-    limite = int(limite)
-    lista_cuadrados = cuadrados_perfectos(limite)
-    print("La lista de cuadrados perfectos es", lista_cuadrados)
-  except:
-    print("Incorrecto")
+  limite = int(input("Introduzca un límite: "))
+  lista_cuadrados = cuadrados_perfectos(limite)
+  print("La lista de cuadrados perfectos es", lista_cuadrados)
+  numero = int(
+    input("Introduzca un numero entero para hacer la raiz cuadrada entera: "))
+  if numero >= 0:
+    raiz = raices(numero)
+    print("La raiz entera es", raiz)
+  else:
+    print("Invalido")
